f8296e149b22b6223b55211de78bc02f77d2dfe7
Abdourahamane <a.sani@alustudent.com>
2022-07-03 05:13:46 -0700

    3-infinite_add.py

--- python-import_modules/3-infinite_add.py
+++ python-import_modules/3-infinite_add.py
@@ -5,10 +5,6 @@ if __name__ == '__main__':
     args_number = len(argv) - 1
 
     if args_number > 0:
-        if args_number == 1:
-            print("{} argument:".format(args_number))
-        else:
-            print("{} arguments:".format(args_number))
         sum_ = 0
         for args in argv[1:]:
             sum_ += int(args)
