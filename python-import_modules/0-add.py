546ab7babc342cffab247a0e847e8d8edd3b6637
Abdourahamane <a.sani@alustudent.com>
2022-07-03 03:40:21 -0700

    add main 2

--- python-import_modules/0-add.py
+++ python-import_modules/0-add.py
@@ -1,5 +1,7 @@
 #!/usr/bin/python3
-add = __import__('add_0.py')
+import add.add
+
 a = 1, b = 2
+
 if __name__ == '__main__':
-    print("{} + {} = {}".format(a, b, add.add(a, b)))
+    print("{} + {} = {}".format(a, b, add(a, b)))
