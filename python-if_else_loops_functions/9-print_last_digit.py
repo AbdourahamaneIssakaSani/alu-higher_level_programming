e316415b5cacc8e7c85ab52aed195999bbfa72b9
Abdourahamane <a.sani@alustudent.com>
2022-07-03 08:02:29 -0700

    again :poop:

--- python-if_else_loops_functions/9-print_last_digit.py
+++ python-if_else_loops_functions/9-print_last_digit.py
@@ -3,5 +3,5 @@ def print_last_digit(number):
     last_digit = abs(number) % 10
     if number < 0:
         last_digit *= (-1)
-    print(last_digit)
+    print("{}".format(last_digit), end="")
     return last_digit
