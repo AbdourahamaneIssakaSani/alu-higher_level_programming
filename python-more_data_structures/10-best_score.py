c25a93711fbe2b35ebb716083ce341dc4bfcf881
Abdourahamane <a.sani@alustudent.com>
2022-07-14 13:27:03 -0700

    10-best_score.py

--- python-more_data_structures/10-best_score.py
+++ python-more_data_structures/10-best_score.py
@@ -2,7 +2,7 @@
 def best_score(a_dictionary):
     if not a_dictionary:
         return None
-    max = list(a_dictionary.items())[0]
+    max = list(a_dictionary.items())[0][1]
     for key, value in a_dictionary.items():
         if value > max:
             max = value
