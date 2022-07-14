9c4c1b5f5a00eea8403a82839e78cdc29b778ed9
Abdourahamane <abdourahamane.issakasani@usmba.ac.ma>
2022-07-14 20:24:39 +0200

    7 ae

--- python-data_structures/7-add_tuple.py
+++ python-data_structures/7-add_tuple.py
@@ -1,6 +1,5 @@
 #!/usr/bin/python3
 def add_tuple(tuple_a=(), tuple_b=()):
-    
     if len(tuple_a) < 2:
         tuple_a += (0,)
     if len(tuple_a) > 2:
@@ -13,4 +12,4 @@ def add_tuple(tuple_a=(), tuple_b=()):
     first_element = tuple_a[0] + tuple_b[0]
     second_element = tuple_a[1] + tuple_b[1]
 
-    return tuple(first_element, second_element)
+    return tuple((first_element, second_element))
