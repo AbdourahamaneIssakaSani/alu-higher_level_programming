9cb49ebc0b1bbddd803925fbf58c4c67c55b0696
Abdourahamane <a.sani@alustudent.com>
2022-07-14 08:17:54 -0700

    6-print_matrix_integer.py

--- python-data_structures/6-print_matrix_integer.py
+++ python-data_structures/6-print_matrix_integer.py
@@ -1,4 +1,6 @@
 #!/usr/bin/python3
 def print_matrix_integer(matrix=[[]]):
     for list_ in matrix:
-        print(*list_)
+        for item in list_:
+            print('{:d}'.format(item), end=" ")
+        print()
