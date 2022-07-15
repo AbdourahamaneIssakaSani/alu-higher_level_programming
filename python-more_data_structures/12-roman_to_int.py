fd135e77ba4ec313c883ad08227ceab56ed450fa
Abdourahamane <a.sani@alustudent.com>
2022-07-15 12:36:54 -0700

    12-roman_to_int.py

--- python-more_data_structures/12-roman_to_int.py
+++ python-more_data_structures/12-roman_to_int.py
@@ -1,3 +1,10 @@
 #!/usr/bin/python3
 def roman_to_int(roman_string):
-    # on ne fait pas d’omelettes sans casser les oeufs, je casse les miens
+    if type(roman_string) != str or roman_string is None:
+        return 0
+    roman_numerals = {"I": 1, "V": 5, "X": 10, "L": 50, "C": 100, "D": 500, "M": 1000}
+    sum_ = 0
+    for n in roman_string:
+        if n in roman_numerals:
+            sum_ += int(roman_numerals[n])
+    return sum_
