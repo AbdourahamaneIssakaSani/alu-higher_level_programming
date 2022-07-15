c66031df6657023f2883df3b9dccbb87192435e2
Abdourahamane <a.sani@alustudent.com>
2022-07-15 14:09:40 -0700

    12-roman_to_int.py

--- python-more_data_structures/12-roman_to_int.py
+++ python-more_data_structures/12-roman_to_int.py
@@ -2,15 +2,27 @@
 def roman_to_int(roman_string):
     if type(roman_string) != str or roman_string is None:
         return 0
-    roman_numerals = {"I": 1, "V": 5, "X": 10, "L": 50, "C": 100, "D": 500, "M": 1000}
+    roman_numerals = {
+        "I": 1,
+        "V": 5,
+        "X": 10,
+        "L": 50,
+        "C": 100,
+        "D": 500,
+        "M": 1000
+    }
     sum_ = 0
     sum_list = list()
-    for i in range(len(roman_string)):
-        try:
-            if roman_string[i] == "I" and (roman_string[i + 1] == "V" or roman_string[i + 1] == "X"):
-                sum_list.append(-int(roman_numerals[roman_string[i]]))
-            else:
-                sum_list.append(int(roman_numerals[roman_string[i]]))
-        except IndexError:
-            pass
+    if len(roman_string) == 1:
+        return roman_numerals[roman_string]
+    if len(roman_string) > 1:
+        for i in range(len(roman_string)):
+            try:
+                if roman_string[i] == "I" and \
+                        (roman_string[i + 1] == "V" or roman_string[i + 1] == "X"):
+                    sum_list.append(-int(roman_numerals[roman_string[i]]))
+                else:
+                    sum_list.append(int(roman_numerals[roman_string[i]]))
+            except IndexError:
+                pass
     return sum(sum_list)
