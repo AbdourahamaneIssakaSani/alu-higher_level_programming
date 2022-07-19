446e28a4e88ca01aae315aad60dce8acdbc41ea6
Abdourahamane <a.sani@alustudent.com>
2022-07-18 21:22:25 -0700

    0-safe_print_list.py

---
+++ python-exceptions/0-safe_print_list.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+def safe_print_list(my_list=[], x=0):
+    for i in range(x):
+        try:
+            print(my_list[i], end="")
+        except IndexError:
+            print()
+            print("nb_print: {:d}".format(i))
+            break
