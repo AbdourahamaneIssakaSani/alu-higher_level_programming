200ad6345aed2a8ae55ffec1abb582cf63f2a4cb
Abdourahamane <a.sani@alustudent.com>
2022-07-18 22:00:41 -0700

    1-safe_print_integer.py

--- python-exceptions/1-safe_print_integer.py
+++ python-exceptions/1-safe_print_integer.py
@@ -3,6 +3,6 @@ def safe_print_integer(value):
     try:
         print("{:d}".format(value))
         return True
-    except TypeError or ValueError:
+    except (TypeError, ValueError):
         pass
     return False
