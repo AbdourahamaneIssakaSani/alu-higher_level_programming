c8ee4969d94b693045f011b3b93d1d0ae8eed082
Abdourahamane <67793634+AbdourahamaneIssakaSani@users.noreply.github.com>
2022-07-23 17:33:42 +0200

    fix debug print

--- python-more_classes/8-rectangle.py
+++ python-more_classes/8-rectangle.py
@@ -69,7 +69,6 @@ class Rectangle:
             value: value of width, must be >=0
         """
         if not isinstance(value, int):
-            print(isinstance(value, int))
             raise TypeError("width must be an integer")
         if value < 0:
             raise ValueError("width must be >= 0")
