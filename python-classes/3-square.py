6423fcc86c2ae4b541ffec72ab4f5c71dd5a3f06
Abdourahamane <67793634+AbdourahamaneIssakaSani@users.noreply.github.com>
2022-07-23 12:54:12 +0200

    3-square.py

---
+++ python-classes/3-square.py
@@ -0,0 +1,20 @@
+#!/usr/bin/python3
+"""Define a square"""
+
+
+class Square:
+    """Creates a square.
+    Private instance attributes: size
+    """
+
+    def __init__(self, size=0):
+        """Initializes the data."""
+        if not isinstance(size, int):
+            raise TypeError("size must be an integer")
+        elif size < 0:
+            raise ValueError("size must be >= 0")
+        self.__size = size
+
+    def area(self):
+        """Return square area."""
+        return self.__size ** 2
