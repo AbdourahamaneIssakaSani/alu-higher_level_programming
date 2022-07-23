1a8eda3f213bdcf39570f1cfbd86a64c5bcc060e
Abdourahamane <a.sani@alustudent.com>
2022-07-23 03:42:44 -0700

    1-square.py

---
+++ python-classes/1-square.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+"""Define a square"""
+
+
+class Square:
+    """Creates a square.
+    Private instance attributes: size
+    """
+    
+
+    def __init__(self, size):
+        """Initializes size"""
+        self.__size = size
