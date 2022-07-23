d7b2f160c3bdf52ff17b1f61c06101cc561e740e
Abdourahamane <67793634+AbdourahamaneIssakaSani@users.noreply.github.com>
2022-07-23 13:29:10 +0200

    fixinnnnnnnnnnn

--- python-classes/6-square.py
+++ python-classes/6-square.py
@@ -70,8 +70,8 @@ class Square:
         if self.__size == 0:
             print()
         else:
-            print('\n'*self.__position[1], end='')
+            print('\n'*self.__position[1], end=' ')
             for n in range(self.__size):
-                print(self.__position[0]*'', end='')
+                print(self.__position[0]*'', end=' ')
                 print(self.__size*'#')
             
