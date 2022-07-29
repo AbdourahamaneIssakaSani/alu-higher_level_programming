c542eafa3fddbee993b9f78b7b7ce3afda9248a8
Abdourahamane <67793634+AbdourahamaneIssakaSani@users.noreply.github.com>
2022-07-29 18:52:33 +0200

    sort

--- python-network_0/6-peak.py
+++ python-network_0/6-peak.py
@@ -3,21 +3,6 @@
 
 
 def find_peak(list_of_integers):
-    """"Finds peak in recursive binary search"""
-    if len(list_of_integers) % 2 == 0:
-        mid = (len(list_of_integers) // 2) - 1
-    else:
-        mid = len(list_of_integers) // 2
-
-    peak = list_of_integers[mid]
-
-    if len(list_of_integers) <= 2:
-        return max(list_of_integers)
-    elif list_of_integers[mid - 1] <= \
-            peak and peak >= \
-            list_of_integers[mid + 1]:
-        return peak
-    elif peak < list_of_integers[mid - 1]:
-        return find_peak(list_of_integers[:mid])
-    else:
-        return find_peak(list_of_integers[mid + 1:])
+    """Finds the peak"""
+    list_of_integers.sort()
+    return list_of_integers[-1]
