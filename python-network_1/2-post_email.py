4b305bf03769002e58bdbe274a5f7ccce876763f
Abdourahamane <67793634+AbdourahamaneIssakaSani@users.noreply.github.com>
2022-08-01 18:22:23 +0200

    Update 2-post_email.py

--- python-network_1/2-post_email.py
+++ python-network_1/2-post_email.py
@@ -14,4 +14,4 @@ if __name__ == '__main__':
     req = urllib.request.Request(url, data)
     with urllib.request.urlopen(req) as response:
         content = response.read()
-        print("Your email is: {}".format(content.decode("utf-8")))
+        print("{}".format(content.decode("utf-8")))
