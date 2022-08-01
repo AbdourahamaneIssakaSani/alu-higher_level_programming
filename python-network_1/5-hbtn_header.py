7d8c7964e87d0a9dad4b326965d69e71fadf38ee
Abdourahamane <a.sani@alustudent.com>
2022-08-01 11:41:10 -0700

    5-hbtn_header.py

--- python-network_1/5-hbtn_header.py
+++ python-network_1/5-hbtn_header.py
@@ -7,4 +7,4 @@ import sys
 if __name__ == '__main__':
     url = sys.argv[1]
     response = requests.get(url)
-    print("{}".format(response.headers["X-Request-Id"] or None))
+    print("{}".format(response.headers.get("X-Request-Id", None))
