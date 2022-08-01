4d053aea2e59fb3b98835f4c6de65cc1ed479696
Abdourahamane <a.sani@alustudent.com>
2022-08-01 11:27:09 -0700

    5-hbtn_header.py

---
+++ python-network_1/5-hbtn_header.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+"""Documented now"""
+import requests
+import sys
+
+
+if __name__ == '__main__':
+    url = sys.argv[1]
+    response = requests.get(url)
+    print("{}".format(response.headers["X-Request-Id"]))
