935aca1292a854710227559b6cbbf491b368fb14
Abdourahamane <67793634+AbdourahamaneIssakaSani@users.noreply.github.com>
2022-08-01 18:18:32 +0200

    Update 2-post_email.py

--- python-network_1/2-post_email.py
+++ python-network_1/2-post_email.py
@@ -4,14 +4,11 @@ import urllib.request
 import urllib.parse
 import sys
 
-
-if __name__ == '__main__':
-    """Documented"""
-    url = sys.argv[1]
-    values = {"email": sys.argv[2]}
-    data = urllib.parse.urlencode(values)
-    data = data.encode('ascii')  # data should be bytes
-    req = urllib.request.Request(url, data)
-    with urllib.request.urlopen(req) as response:
-        content = response.read()
-        print("Your email is: {}".format(content.decode("utf-8"))
+url = sys.argv[1]
+values = {"email": sys.argv[2]}
+data = urllib.parse.urlencode(values)
+data = data.encode('ascii')  # data should be bytes
+req = urllib.request.Request(url, data)
+with urllib.request.urlopen(req) as response:
+    content = response.read()
+    print("Your email is: {}".format(content.decode("utf-8"))
