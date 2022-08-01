3db2568040334842a6a8c55fbb745b7bc2dcbd44
Abdourahamane <67793634+AbdourahamaneIssakaSani@users.noreply.github.com>
2022-08-01 21:37:09 +0200

    Update 8-json_api.py

--- python-network_1/8-json_api.py
+++ python-network_1/8-json_api.py
@@ -5,12 +5,12 @@ import sys
 
 if __name__ == '__main__':
     params = sys.argv[1]
-    response = requests.get("http://0.0.0.0:5000/search_user?q={}".format(params))
+    response = requests.post("http://0.0.0.0:5000/search_user", data={"q": params})
     try:
         json_response = response.json()
         if response.headers.get("Content-Type") == 'application/json':
             print("[{}] {}".format(json_response["id"], json_response["name"]))
         else:
-            print("{}".format(response.text))
-    except requests.exceptions.JSONDecodeError:
+            print("No result")
+    except:
         print("Not a valid JSON")
