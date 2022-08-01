28bf378d3861ba0a722bfb360369b21a4ecee1d4
Abdourahamane <a.sani@alustudent.com>
2022-08-01 07:21:51 -0700

    2-post_email.py

---
+++ python-network_1/2-post_email.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+"""I documented you"""
+
+import urllib.request
+import urllib.parse
+import sys
+
+if __name__ == '__main__':
+    url = sys.argv[1]
+    values = {"email": sys.argv[2]}
+    data = urllib.parse.urlencode(values)
+    data = data.encode('ascii')  # data should be bytes
+    req = urllib.request.Request(url, data)
+    with urllib.request.urlopen(req) as response:
+        content = response.read()
+        print("Your email is: {}".format(content))
