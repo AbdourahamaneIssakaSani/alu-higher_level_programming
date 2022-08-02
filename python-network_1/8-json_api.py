df7bf9777c7f8cd24fad5d7417f1acae7ccd0473
Abdourahamane <67793634+AbdourahamaneIssakaSani@users.noreply.github.com>
2022-08-02 09:56:33 +0200

    Update 8-json_api.py

--- python-network_1/8-json_api.py
+++ python-network_1/8-json_api.py
@@ -12,7 +12,9 @@ if __name__ == '__main__':
      try:
         json_response = response.json()
         if response.headers.get("Content-Type") == 'application/json':
-            print("[{}] {}".format(json_response["id"], json_response["name"]))\
-                if len(json_response) > 0 else print("No result")
+            if len(json_response) > 0:
+                print("[{}] {}".format(json_response["id"], json_response["name"]))
+            else:
+                print("No result")
     except:
         print("Not a valid JSON")
