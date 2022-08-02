8f8691a9591bf026be4c1ea6601f057cbb4c63d6
Abdourahamane <67793634+AbdourahamaneIssakaSani@users.noreply.github.com>
2022-08-02 11:25:16 +0200

    Update 10-my_github.py

--- python-network_1/10-my_github.py
+++ python-network_1/10-my_github.py
@@ -5,11 +5,13 @@ import requests.auth
 import sys
 
 if __name__ == '__main__':
+    username = sys.argv[1]
+    password = sys.argv[2]
     response = requests.get(
         url="https://api.github.com/user",
         auth=(requests.auth.HTTPBasicAuth(
-            "AbdourahamaneIssakaSani",
-            "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"
+            username,
+            password
         )))
     try:
         json_response = response.json()
