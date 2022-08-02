f58535b4601f0a9ee9952f3626de393bb01ef4bc
Abdourahamane <67793634+AbdourahamaneIssakaSani@users.noreply.github.com>
2022-08-02 10:02:33 +0200

    Update 8-json_api.py

--- python-network_1/8-json_api.py
+++ python-network_1/8-json_api.py
@@ -8,12 +8,18 @@ if __name__ == '__main__':
         params = sys.argv[1]
     except IndexError:
         params = ""
-    response = requests.post("http://0.0.0.0:5000/search_user", data={"q": params})
+    response = requests.post(
+        "http://0.0.0.0:5000/search_user",
+        data={"q": params}
+    )
     try:
         json_response = response.json()
         if response.headers.get("Content-Type") == 'application/json':
             if len(json_response) > 0:
-                print("[{}] {}".format(json_response["id"], json_response["name"]))
+                print("[{}] {}".format(
+                    json_response["id"],
+                    json_response["name"])
+                )
             else:
                 print("No result")
     except:
