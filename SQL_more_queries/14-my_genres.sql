0a38a1e0df54b76c16c9f73c89a9958baf2c2a6b
Abdourahamane <a.sani@alustudent.com>
2022-08-05 02:14:23 -0700

    automated commit of 14-my_genres.sql

---
+++ SQL_more_queries/14-my_genres.sql
@@ -0,0 +1,6 @@
+-- 14-my_genres.sql
+SELECT tv_genres.name FROM tv_genres
+JOIN tv_show_genres ON tv_genres.id = tv_show_genres.genre_id
+JOIN tv_shows ON tv_shows.id = tv_shows.show_id
+WHERE tv_shows.title = 'Dexter'
+ORDER BY tv_genres.name ASC;
