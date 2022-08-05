12191ba1c0153d9f08c63f1753d20958c3c8a97a
Abdourahamane <a.sani@alustudent.com>
2022-08-05 02:09:51 -0700

    automated commit of 13-count_shows_by_genre.sql

--- SQL_more_queries/13-count_shows_by_genre.sql
+++ SQL_more_queries/13-count_shows_by_genre.sql
@@ -1,6 +1,6 @@
 -- 13-count_shows_by_genre.sql
 SELECT tv_genres.name AS genre, COUNT(tv_show_genres.genre_id) AS number_of_shows
 FROM tv_genres, tv_show_genres
-WHERE a.id = b.genre_id
+WHERE tv_genres.id = tv_show_genres.genre_id
 GROUP BY tv_show_genres.genre_id
 ORDER BY number_of_shows DESC;
