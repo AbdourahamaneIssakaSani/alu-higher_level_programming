f2c489edc449c4263be7415abe08d248b2b14945
Abdourahamane <a.sani@alustudent.com>
2022-08-05 01:53:39 -0700

    automated commit of 11-genre_id_all_shows.sql

--- SQL_more_queries/11-genre_id_all_shows.sql
+++ SQL_more_queries/11-genre_id_all_shows.sql
@@ -1,5 +1,5 @@
 -- 11-genre_id_all_shows.sql
 SELECT tv_shows.title, tv_show_genres.genre_id
-FROM tv_shows a
+FROM tv_shows, tv_show_genres
 WHERE tv_shows.id = tv_show_genres.show_id
 ORDER BY tv_shows.title ASC, tv_show_genres.genre_id ASC;
