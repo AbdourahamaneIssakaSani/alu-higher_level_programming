f5142755b398532349c954ecdc70ccf3bc4f2a35
Abdourahamane <a.sani@alustudent.com>
2022-08-05 01:39:05 -0700

    automated commit of 8-cities_of_california_subquery.sql

---
+++ SQL_more_queries/8-cities_of_california_subquery.sql
@@ -0,0 +1,6 @@
+-- 8-cities_of_california_subquery.sql
+SELECT cities.id, cities.name
+FROM cities, states
+WHERE cities.state_id = states.id
+    AND states.name = 'California'
+    ORDER BY cities.id ASC;
