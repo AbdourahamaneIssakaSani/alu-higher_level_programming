1cefa68ec3e29713e199877650ac0b8628cedc8f
Abdourahamane <67793634+AbdourahamaneIssakaSani@users.noreply.github.com>
2022-08-14 14:03:16 +0200

    Update 4-print_square.py

--- python-test_driven_development/4-print_square.py
+++ python-test_driven_development/4-print_square.py
@@ -4,10 +4,12 @@
 
 def print_square(size):
     """"Doc"""
-    if size <= 0:
-        raise ValueError("size must be >= 0")
+    
     if not isinstance(size, (int,)):
         raise TypeError("size must be an integer")
+    
+    if size < 0:
+        raise ValueError("size must be >= 0")
 
     for _ in range(size):
         print("#" * size)
