0475d358b0660f7df2d0178ab1e8ecc07b229662
Abdourahamane <a.sani@alustudent.com>
2022-08-14 05:55:29 -0700

    automated commit of 5-text_indentation.py

---
+++ python-test_driven_development/5-text_indentation.py
@@ -0,0 +1,19 @@
+#!/usr/bin/python3
+""""Doc"""
+
+
+def text_indentation(text):
+    """"Doc"""
+    if not isinstance(text, (str,)):
+        raise TypeError("text must be a string")
+
+    characters = ['.', '?', ':']
+    for i in range(len(text)):
+        if text[i] in characters:
+            try:
+                if text[i + 1] == " ":
+                    text = text[:i + 1] + text[i + 1:]
+                text = text[:i + 1] + "\n\n" + text[i + 1:]
+            except IndexError:
+                pass
+    print(text)
