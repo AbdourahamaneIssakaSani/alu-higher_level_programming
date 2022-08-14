b00898ff15fd49de049463f82de61a6b54c95a59
Abdourahamane <67793634+AbdourahamaneIssakaSani@users.noreply.github.com>
2022-08-14 10:49:11 +0200

    Update 0-add_integer.py

--- python-test_driven_development/0-add_integer.py
+++ python-test_driven_development/0-add_integer.py
@@ -12,7 +12,7 @@ def add_integer(a, b=98):
     if type(a) == float or type(b) == float:
         a = int(a)
         b = int(b)
-        
+
     if type(a) != int:
         raise(TypeError, "a must be an integer")
     if type(b) != int:
