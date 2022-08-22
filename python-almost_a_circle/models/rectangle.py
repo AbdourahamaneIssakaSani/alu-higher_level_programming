0bf5322b2110bc155885ddc67abcc43f1220abb9
Abdourahamane <a.sani@alustudent.com>
2022-08-22 11:18:47 -0700

    automated commit of models/rectangle.py

---
+++ python-almost_a_circle/models/rectangle.py
@@ -0,0 +1,63 @@
+#!/usr/bin/python3
+from models.base import Base
+
+"""Class Rectangle that inherit from Base"""
+
+
+class Rectangle(Base):
+    """"
+        Class Rectangle inheriting Base
+        Attr :
+                id: number
+                width: number
+                height: number
+                x: number
+                y: number
+    """
+
+    def __init__(self, width, height, x=0, y=0, id=None):
+        super.__init__(id)
+        self.width = width
+        self.height = height
+        self.x = x
+        self.y = y
+
+    @property
+    def width(self):
+        """Width getter"""
+        return self.__width
+
+    @width.setter
+    def width(self, value):
+        """Width setter"""
+        self.__width = value
+
+    @property
+    def height(self):
+        """Height getter"""
+        return self.__height
+
+    @height.setter
+    def height(self, value):
+        """Height setter"""
+        self.__height = value
+
+    @property
+    def x(self):
+        """x getter"""
+        return self.__x
+
+    @x.setter
+    def x(self, value):
+        """x setter"""
+        self.__x = value
+
+    @property
+    def y(self):
+        """x getter"""
+        return self.__y
+
+    @y.setter
+    def y(self, value):
+        """x setter"""
+        self.__y = value
