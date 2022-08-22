272756ac947d964616b82f6bf9ad157ac9da06e9
Abdourahamane <a.sani@alustudent.com>
2022-08-22 10:51:34 -0700

    automated commit of models/

---
+++ python-almost_a_circle/models/base.py
@@ -0,0 +1,28 @@
+#!/usr/bin/python3
+"""Doc"""
+
+
+class Base:
+    """"
+        Class Base
+        Attr :
+                id: number
+        """
+    __nb_objects = 0
+
+    def __init__(self, id=None):
+        self.id = id
+
+    @property
+    def id(self):
+        """Doc"""
+        return self.__id
+
+    @id.setter
+    def id(self, value):
+        """Doc"""
+        if value is None:
+            self.__nb_objects += 1
+            self.__id = self.__nb_objects
+        else:
+            self.__id = value
