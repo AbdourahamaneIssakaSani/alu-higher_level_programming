9cda7911c6702b49187a336bd77eb4685f239c2d
Abdourahamane <67793634+AbdourahamaneIssakaSani@users.noreply.github.com>
2022-08-23 16:10:28 +0200

    Update base.py

--- python-almost_a_circle/models/base.py
+++ python-almost_a_circle/models/base.py
@@ -30,7 +30,8 @@ class Base:
     @staticmethod
     def to_json_string(list_dictionaries):
         """Doc"""
-        if list_dictionaries is not None:
-            return json.dumps(list_dictionaries)
-        else:
+        if list_dictionaries is None or \
+                len(list_dictionaries) == 0:
             return '"[]"'
+        else:
+            return json.dumps(list_dictionaries)
