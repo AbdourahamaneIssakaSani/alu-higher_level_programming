b28d3f2aa4780c81346a0a1a943f26c8a92af641
Abdourahamane <67793634+AbdourahamaneIssakaSani@users.noreply.github.com>
2022-08-23 12:23:55 +0200

    Update square.py

--- python-almost_a_circle/models/square.py
+++ python-almost_a_circle/models/square.py
@@ -30,5 +30,5 @@ class Square(Rectangle):
 
     def __str__(self):
         """return [Square] (<id>) <x>/<y> - <size>"""
-        return "[Square] ({}) <{}>/<{}> - {}" \
-            .format(self.id, self.x, self.y, self.size)
+        return "[{}] ({}) <{}>/<{}> - {}" \
+            .format(self.__class__.__name__, self.id, self.x, self.y, self.size)
