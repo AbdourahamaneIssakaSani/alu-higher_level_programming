1f6ec6a3d8e756242a3fe2bef7085de519fec605
Abdourahamane <a.sani@alustudent.com>
2022-08-23 07:13:24 -0700

    automated commit of models/base.py

--- python-almost_a_circle/models/base.py
+++ python-almost_a_circle/models/base.py
@@ -29,7 +29,7 @@ class Base:
 
     @staticmethod
     def to_json_string(list_dictionaries):
-        """Doc"""
+        """returns the JSON string representation of list_dictionaries"""
         if list_dictionaries is None or \
                 len(list_dictionaries) == 0:
             return '"[]"'
