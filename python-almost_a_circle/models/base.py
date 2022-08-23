186c9a2236812eee5a95360a00a4250f644d5688
Abdourahamane <67793634+AbdourahamaneIssakaSani@users.noreply.github.com>
2022-08-23 16:17:21 +0200

    fix :clown_face:

--- python-almost_a_circle/models/base.py
+++ python-almost_a_circle/models/base.py
@@ -34,6 +34,6 @@ class Base:
         """returns the JSON string representation of list_dictionaries"""
         if list_dictionaries is None or \
                 len(list_dictionaries) == 0:
-            return '"[]"'
+            return "[]"
         else:
             return json.dumps(list_dictionaries)
