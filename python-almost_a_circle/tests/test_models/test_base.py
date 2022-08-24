6c1c5a2e15b5c376d1fb577f84d090a6dca89d80
Abdourahamane <a.sani@alustudent.com>
2022-08-24 07:11:09 -0700

    automated commit of tests/test_models/test_base.py

---
+++ python-almost_a_circle/tests/test_models/test_base.py
@@ -0,0 +1,37 @@
+#!/usr/bin/python3
+"""Test cases for Base"""
+
+import unittest
+
+from models.base import Base
+
+
+class TestBase(unittest.TestCase):
+    """Test class for Base"""
+
+    def basic_test(self):
+        """Doc"""
+        base = Base()
+        base_1 = Base()
+        base_89 = Base(89)
+        self.assertEqual(base.id, 1)
+        self.assertEqual(base_1.id, 2)
+        self.assertEqual(base_89.id, 89)
+
+    def test_to_json_string(self):
+        """Doc"""
+        self.assertEqual(Base.to_json_string(None), "[]")
+        self.assertEqual(Base.to_json_string([]), "[]")
+        self.assertEqual(Base.to_json_string([{'id': 12}]), '[{"id": 12}]')
+        self.assertEqual(type(Base.to_json_string([{'id': 12}])), str)
+
+    def test_from_json_string(self):
+        """Doc"""
+        self.assertEqual(Base.from_json_string(None), [])
+        self.assertEqual(Base.from_json_string("[]"), [])
+        self.assertEqual(Base.from_json_string('[{"id": 89}]'), [{'id': 89}])
+        self.assertEqual(type(Base.from_json_string('[{"id": 89}]')), list)
+
+
+if __name__ == '__main__':
+    unittest.main()
