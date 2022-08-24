696590650a8db149db1ac281e7a99a654ead1737
Abdourahamane <67793634+AbdourahamaneIssakaSani@users.noreply.github.com>
2022-08-24 08:53:06 +0200

    the dummy thingy

--- python-almost_a_circle/models/base.py
+++ python-almost_a_circle/models/base.py
@@ -58,3 +58,13 @@ class Base:
             return list()
         else:
             return json.loads(json_string)
+
+    @classmethod
+    def create(cls, **dictionary):
+        """ returns an instance with all attributes already set"""
+        if cls.__name__ == "Rectangle":
+            dummy_instance = cls(4, 3)
+        if cls.__name__ == "Square":
+            dummy_instance = cls(4)
+        dummy_instance.update(**dictionary)
+        return dummy_instance
