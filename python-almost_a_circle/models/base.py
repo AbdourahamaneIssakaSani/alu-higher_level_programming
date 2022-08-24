bddb8c0668b1f3cf2d70bc41f194077b6496d3e6
Abdourahamane <67793634+AbdourahamaneIssakaSani@users.noreply.github.com>
2022-08-24 07:36:46 +0200

    else only

--- python-almost_a_circle/models/base.py
+++ python-almost_a_circle/models/base.py
@@ -47,7 +47,7 @@ class Base:
         with open(cls.__name__ + '.json', "w") as file:
             if list_objs is None or len(list_objs) == 0:
                 file.write('[]')
-            elif type(list_objs) == list:
+            else:
                 for obj in list_objs:
                     file.write(cls.to_json_string([obj.to_dictionary()]))
 
