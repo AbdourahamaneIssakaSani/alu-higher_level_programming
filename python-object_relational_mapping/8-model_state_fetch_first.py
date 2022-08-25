8401bf771511a2ed54fbb21980ade00d29aa20e3
Abdourahamane <67793634+AbdourahamaneIssakaSani@users.noreply.github.com>
2022-08-25 12:49:05 +0200

    Update 8-model_state_fetch_first.py

--- python-object_relational_mapping/8-model_state_fetch_first.py
+++ python-object_relational_mapping/8-model_state_fetch_first.py
@@ -16,7 +16,7 @@ if __name__ == "__main__":
 
     record = session.query(State).first()
 
-    print("{}: {}".format(record['id'], record['name'])) if record \
+    print("{}: {}".format(record.id, record.name) if record \
         else print("Nothing")
 
     session.close()
