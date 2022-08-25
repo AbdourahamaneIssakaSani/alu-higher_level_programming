1835e3520daf2652d13404099fc10c983f8d814e
Abdourahamane <a.sani@alustudent.com>
2022-08-25 01:27:13 -0700

    automated commit of 0-select_states.py

---
+++ python-object_relational_mapping/0-select_states.py
@@ -0,0 +1,26 @@
+#!/usr/bin/python3
+""" lists all states from the database hbtn_0e_0_usa"""
+
+import MySQLdb
+import sys
+
+try:
+    db = MySQLdb.connect(host="localhost",
+                         port=3306,
+                         user=sys.argv[1],
+                         passwd=sys.argv[2],
+                         db=sys.argv[3]
+                         )
+except:
+    print("Connection error")
+
+cursor = db.cursor()
+
+cursor.execute("SELECT * FROM states ORDER BY id ASC")
+
+records = cursor.fetchall()
+for data in records:
+    print(data)
+
+cursor.close()
+db.close()
