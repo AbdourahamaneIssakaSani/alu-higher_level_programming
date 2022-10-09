404b6417aa366b0c5ed97a8a2340e149fb0fa59b
Abdourahamane <a.sani@alustudent.com>
2022-10-09 06:07:24 -0700

    automated :robot: commit of 6-multi_languages_loop.js :technologist:

--- javascript-warm_up/6-multi_languages_loop.js
+++ javascript-warm_up/6-multi_languages_loop.js
@@ -1,6 +1,6 @@
 #!/usr/bin/node
 
-let tx = ['C is fun', 'Python is cool', 'Javascript is amazing'];
+const tx = ['C is fun', 'Python is cool', 'Javascript is amazing'];
 
 for (let i = 0; i < tx.length; i++) {
   console.log(tx[i]);
