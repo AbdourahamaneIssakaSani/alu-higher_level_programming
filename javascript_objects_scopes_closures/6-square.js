3bb75cd26f18e6ef73798f342aa582c3dc1057e0
Abissa <a.sani@alustudent.com>
2022-10-12 11:14:58 -0700

    :robot: automated commit of 6-square.js

--- javascript_objects_scopes_closures/6-square.js
+++ javascript_objects_scopes_closures/6-square.js
@@ -1,8 +1,8 @@
 #!/usr/bin/node
 
-module.exports = class Square extends require("./5-square") {
+module.exports = class Square extends require('./5-square') {
   charPrint (c) {
-    const char = c || undefined;
+    const char = c || 'X';
     for (let i = 0; i < this.height; i++) {
       console.log(char.repeat(this.width));
     }
