63e2049148fc47bf5f4f09fc172f93044a1b022c
Abissa <a.sani@alustudent.com>
2022-10-13 21:49:39 -0700

    filename fix

--- javascript-web_scraping/5-request_store.js
+++ javascript-web_scraping/5-request_store.js
@@ -2,7 +2,7 @@
 const req = require('request');
 const fs = require('fs');
 const url = process.argv[2];
-const file = process.argv[2];
+const file = process.argv[3];
 req.get(url, async (err, res) => {
   err
     ? console.log(err)
