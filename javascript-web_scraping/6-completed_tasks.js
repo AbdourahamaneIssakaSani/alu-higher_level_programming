2ff0a07197d30929ebaa9a3a288348a399b5af4a
Abissa <a.sani@alustudent.com>
2022-10-14 01:44:15 -0700

    :robot: automated commit of 6-completed_tasks.js

--- javascript-web_scraping/6-completed_tasks.js
+++ javascript-web_scraping/6-completed_tasks.js
@@ -1,7 +1,7 @@
 #!/usr/bin/node
 const req = require('request');
 
-function getCompletedTasks (data = [], userId) {
+function getCompletedTasks (data, userId) {
   let count = 0;
   data
     .filter((element) => element.userId === userId)
