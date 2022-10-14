368a29f784ab147b1216383f4acd9c26206a3aaf
Abissa <a.sani@alustudent.com>
2022-10-13 20:52:20 -0700

    fix ternary ops and standard

--- javascript-web_scraping/1-writeme.js
+++ javascript-web_scraping/1-writeme.js
@@ -1,6 +1,6 @@
 #!/usr/bin/node
-const fs = require("fs");
+const fs = require('fs');
 
 fs.writeFile(process.argv[2], process.argv[3], (err) => {
-  err ? console.log(err) : "";
+  if (err) console.log(err);
 });
